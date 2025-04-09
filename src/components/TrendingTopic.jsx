f062f0881d58560b548077b8f1def0964591e6bb
Amer Salih Bugshan <amer.s.bugshan@gmail.com>
2025-04-09 21:33:02 +0300

    done

--- src/components/TrendingTopic.jsx
+++ src/components/TrendingTopic.jsx
@@ -1,9 +1,9 @@
-const TrendingTopic = () => {
-    return (
-      <div className="bg-gray-100 px-4 py-2 rounded-full mb-2 text-center">
-        <span>{topic}</span>
-      </div>
-    );
-  };
+const TrendingTopic = ({topic}) => {
+  return (
+    <div className="bg-gray-100 px-4 py-2 rounded-full mb-2 text-center">
+      <span>{topic}</span>
+    </div>
+  );
+};
 
-  export default TrendingTopic;
+export default TrendingTopic;
